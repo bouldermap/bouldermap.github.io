177031d0f9cf3c79960677b2f9379df568123514
German <givicencio@gmail.com>
2025-11-10 11:45:50 -0300

    -

---
+++ config.js
@@ -0,0 +1,7 @@
+
+angular.module('bouldermapApp').config(['constants', function(constants) {
+    
+    constants.licenseName = "bouldermap";
+    constants.api = "https://bouldermap.azurewebsites.net";
+    
+}]);
